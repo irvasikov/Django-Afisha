bd2e9899d67dfeaf3f61b949535d39c819e8f8e6
Ildar Vasikov <ir.vasikov@gmail.com>
2021-07-04 09:45:31 +0500

    Add Places model

--- afisha/yandex_afisha/models.py
+++ afisha/yandex_afisha/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Places(models.Model):
+    title = models.CharField("Название", max_length=200)
+    description_short = models.TextField("Краткое описание")
+    description_long = models.TextField("Основной текст")
+    lng = models.DecimalField("Долгота", max_digits=16, decimal_places=14)
+    lat = models.DecimalField("Широта", max_digits=16, decimal_places=14)
+
