0b12ece1a3d01b34a59e5ffe4f21e600a0b4a45a
Ildar Vasikov <ir.vasikov@gmail.com>
2021-07-09 17:00:02 +0500

    Add new function image_shot in admin.ModelAdmin

--- afisha/yandex_afisha/admin.py
+++ afisha/yandex_afisha/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Places, Images
 
 
 class ImageInline(admin.TabularInline):
     model = Images
+    readonly_fields = ("image_shot",)
 
+    def image_shot(self, obj):
+        return format_html("<img src=\"{}\" width={} height={} />",
+                           obj.image.url,
+                           "auto",
+                           "200",
+                           )
 
 class PlacesAdmin(admin.ModelAdmin):
     inlines = [
@@ -14,7 +22,10 @@ class PlacesAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Places
-    pass
+
+
+# class ImagesAdmin(admin.ModelAdmin):
+
 
 
 admin.site.register(Places, PlacesAdmin)
